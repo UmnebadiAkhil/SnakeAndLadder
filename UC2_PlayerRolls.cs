d800059346d3d48bbf671021e4bd30cc71650510
Akhil <akhilu5042@gmail.com>
2022-01-11 13:44:47 +0530

    UC-2

---
+++ UC2_PlayerRolls.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class UC2_PlayerRolls
+    {
+            public int playerPosition = 0;
+            Random random = new Random();
+            public int DieRoll()
+            {
+                int roll = random.Next(1, 7);
+                return roll;
+            }
+    }
+}
