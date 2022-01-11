d4d7c3990a955360ce5e22037b98cd05705af63e
Akhil <akhilu5042@gmail.com>
2022-01-11 13:45:32 +0530

    UC-3

---
+++ UC3_PlayerRollsADie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class UC3_PlayerRollsADie
+    {
+        public const int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
+        public int playerPosition = 0;
+        Random random = new Random();
+        public int DieRolling()
+        {
+            int roll = random.Next(1, 7);
+            return roll;
+        }
+        public void PlayerCheck()
+        {
+            int die = DieRolling();
+            int option = random.Next(0, 3);
+            switch (option)
+            {
+                case NO_PLAY:
+                    break;
+                case LADDER:
+                    playerPosition += die;
+                    break;
+                case SNAKE:
+                    playerPosition -= die;
+                    break;
+            }
+            Console.WriteLine(playerPosition);
+        }
+    }
+}
+
