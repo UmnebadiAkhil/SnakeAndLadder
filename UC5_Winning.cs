698f057c77f349fc6d6591a1b3460f2c6b11a647
Akhil <akhilu5042@gmail.com>
2022-01-11 13:47:32 +0530

    UC-5

---
+++ UC5_Winning.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class UC5_Winning
+    {
+        public const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, WINNING = 100, START = 0;
+        public int PlayerPosition = 0;
+        Random random = new Random();
+
+        public int DieRolling()
+        {
+            int roll = random.Next(1, 7);
+            return roll;
+        }
+        public void PlayerCheck()
+        {
+            while (PlayerPosition <= WINNING)
+            {
+                int dieRoll = DieRolling();
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case NO_PLAY:
+                        break;
+                    case LADDER:
+                        PlayerPosition += dieRoll;
+                        if (PlayerPosition > WINNING)
+                        {
+                            PlayerPosition += dieRoll;
+                        }
+                        Console.WriteLine("Got Ladder");
+                        break;
+                    case SNAKE:
+                        PlayerPosition -= dieRoll;
+                        if (PlayerPosition < START)
+                        {
+                            PlayerPosition = START;
+                        }
+                        Console.WriteLine("Got Snake");
+                        break;
+                }
+                
+                Console.WriteLine("The Player Position is :" + PlayerPosition + " with a roll of " + dieRoll);
+                
+            }
+            Console.WriteLine("The Player wins the Game");
+        }
+    }
+}
