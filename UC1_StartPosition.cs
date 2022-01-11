c3aa6c1a1461cef2abe678bcbf29287594ed0813
Akhil <akhilu5042@gmail.com>
2022-01-11 13:42:59 +0530

    UC-1

---
+++ UC1_StartPosition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class UC1_StartPosition
+    {
+        public void StartPosition()
+        {
+            int START_POSITION = 0;
+            Console.WriteLine("Single player at start position " + START_POSITION);
+        }
+    }
+}
